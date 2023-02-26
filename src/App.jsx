6632895c00969b72ccfff336fb62e0b6b073c413
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-26 10:34:00 +0700

    try to use MyButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import MyButton from './components/MyButton'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <MyButton/>
     </div>
   )
 }
