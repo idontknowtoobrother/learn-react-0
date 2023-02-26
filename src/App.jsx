ba3e01e063efb3db043e17c1cac9516791713c1c
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-26 10:52:11 +0700

    done: use variable in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import MyButton from './components/MyButton'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const user = {
+    fullname: "Jakkrit Chaopron",
+    imgUrl: "https://media.discordapp.net/attachments/909065818237718608/1079248174784467104/270040687_734421277530170_7796251460781359228_n.jpg",
+    imgSize: 150
+  }
+
   return (
-    <div className="App">
-      <MyButton/>
-    </div>
+    // React Fragment
+    <>  
+      <h2>{user.fullname}</h2>
+      <img className='avatar' src={user.imgUrl} width={user.imgSize} alt={'Photo of '+user.fullname} />
+    </>
   )
+
 }
 
 export default App
