34f61d2e0124b2f3002a9fe6bd608f3b80765824
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-26 11:13:22 +0700

    list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,31 @@ function App() {
     imgSize: 150
   }
 
+  const users = [
+    {
+      id: 0,
+      fullname: "cos9iwo",
+      imgUrl: "https://media.discordapp.net/attachments/909065818237718608/1079248174784467104/270040687_734421277530170_7796251460781359228_n.jpg",
+    },
+    {
+      id: 1,
+      fullname: "lafiesta",
+      imgUrl: "https://media.discordapp.net/attachments/909065818237718608/1079248174784467104/270040687_734421277530170_7796251460781359228_n.jpg",
+    },
+    {
+      id: 2,
+      fullname: "killpGOD",
+      imgUrl: "https://media.discordapp.net/attachments/909065818237718608/1079248174784467104/270040687_734421277530170_7796251460781359228_n.jpg",
+    },
+  ]
+  const listProfile = users.map(user => 
+    <li key={user.id}>
+      <h3>{user.fullname}</h3>
+      <img className='avatar' src={user.imgUrl} width={64} alt={'Photo of '+user.fullname} />
+    </li>
+  ) 
+
+
   let isShow = true
 
   return (
@@ -25,9 +50,10 @@ function App() {
       }
 
       {isShow && <MyButton/>}
-      
+
       <h2>{user.fullname}</h2>
       <img className='avatar' src={user.imgUrl} width={user.imgSize} alt={'Photo of '+user.fullname} />
+      {listProfile}
     </>
   )
 
